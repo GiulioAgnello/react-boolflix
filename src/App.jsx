60c6da468753228abb258fd218102a053ad602ef
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-05 12:00:50 +0200

    Layout_scaffholding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+
 import Main from "./components/Main";
 
 export default function App() {
@@ -8,7 +8,6 @@ export default function App() {
     <>
       <Header />
       <Main />
-      <Footer />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,22 @@
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand">BOOLFLIX</a>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search a movie..."
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </>
+  );
 }
