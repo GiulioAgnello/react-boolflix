7810e37a2072bf37314c59df826c965e90ff5fea
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-05 17:03:04 +0200

    searchWork

--- src/Context/MovieContext.jsx
+++ src/Context/MovieContext.jsx
@@ -2,23 +2,30 @@ import axios from "axios";
 import { createContext, useState, useEffect, useContext } from "react";
 
 const MovieContext = createContext();
-
+const API_URL = "https://api.themoviedb.org/3";
+const api_key = "1e77a6c1b6a58f585e346407db92f6d6";
 const MovieProvider = ({ children }) => {
   const [movies, setMovie] = useState([]);
+  const [tvseries, setTvseries] = useState([]);
 
-  function fetchmovie() {
+  function fetchmovie(param) {
     axios
-      .get(
-        "https://api.themoviedb.org/3/search/movie?api_key=1e77a6c1b6a58f585e346407db92f6d6&query=spider"
-      )
+      .get(`${API_URL}/search/movie?api_key=${api_key}&query=${param}`)
       .then((res) => {
-        console.log(res.data.results);
-
         setMovie(res.data.results);
       });
   }
+
+  function fetchTvSeries() {
+    axios
+      .get(`${API_URL}/search/movie?api_key=${api_key}&query=spider`)
+      .then((res) => {
+        setTvseries(res.data.results);
+      });
+  }
+
   useEffect(fetchmovie, []);
-  const movieData = { movies };
+  const movieData = { movies, fetchmovie, tvseries, fetchTvSeries };
   return (
     <MovieContext.Provider value={movieData}>{children}</MovieContext.Provider>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
+import { useMovies } from "../Context/MovieContext";
+
 export default function Header() {
+  const { fetchmovie } = useMovies();
+
+  const [movieserch, setSerchmovie] = useState("");
+
+  function hedlerSearch(e) {
+    e.preventDefault();
+    console.log(movieserch);
+    fetchmovie(movieserch);
+  }
   return (
     <>
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand">BOOLFLIX</a>
-          <form className="d-flex">
+          <form className="d-flex" onSubmit={hedlerSearch}>
             <input
               className="form-control me-2"
-              type="search"
+              type="text"
+              onChange={(e) => setSerchmovie(e.target.value)}
               placeholder="Search a movie..."
-              aria-label="Search"
+              aria-label="text"
             />
             <button className="btn btn-outline-success" type="submit">
               Search
